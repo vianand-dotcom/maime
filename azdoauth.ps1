7b82ba068032091ad867328f264dc96047bc98c5
Vishal Anand <vishal.anand225@gmail.com>
2023-02-14 11:28:20 +0530

    Create azdoauth.ps1

---
+++ azdoauth.ps1
@@ -0,0 +1,3 @@
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$(Get-Content ${Env:AZP_TOKEN_FILE})"))
+$package = Invoke-RestMethod -Headers @{Authorization=("Basic $base64AuthInfo")} "$(${Env:AZP_URL})/_apis/distributedtask/packages/agent?platform=win-x64&`$top=1"
+$packageUrl = $package[0].Value.downloadUrl
